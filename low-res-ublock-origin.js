0cf0c7136a8284aabe95479347b11295cab38e24
ErectusTestus <54259247+ErectusTestus@users.noreply.github.com>
2022-12-24 08:00:46 -0600

    Update low-res-ublock-origin.js

--- low-res-ublock-origin.js
+++ low-res-ublock-origin.js
@@ -2,7 +2,7 @@ twitch-videoad.js application/javascript
 (function() {
     if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
     function hookFetch() {
-        var OPT_ACCESS_TOKEN_PLAYER_TYPE = 'thunderdome';//480p
+        var OPT_ACCESS_TOKEN_PLAYER_TYPE = 'pop_tart';//???
         //var OPT_ACCESS_TOKEN_PLAYER_TYPE = 'picture-by-picture';//360p
         var realFetch = window.fetch;
         window.fetch = function(url, init, ...args) {
